0224dc14006c44ff2368282b1cdbc7fa45f41629
Katie Huntley <kah294@cornell.edu>
2023-12-07 11:13:20 -0500

    generic terrain with black background

--- src/FinalProject/Main/Scene/MainSceneController.ts
+++ src/FinalProject/Main/Scene/MainSceneController.ts
@@ -1,80 +1,30 @@
-/**
- * @file Main scene controller for your application
- * @description This is where you connect models to views.
- * This is done mainly by defining your model view spec and interaction modes.
- */
-import { MainSceneModel } from "./MainSceneModel";
-import { ADragInteraction, AGLContext, AInteractionEvent, AKeyboardInteraction, Color } from "../../../anigraph";
-import { BaseSceneController } from "../../HelperClasses";
-import { CustomNode1Model, CustomNode1View } from "../Nodes/CustomNode1";
-import { ADebugInteractionMode } from "../../../anigraph/starter";
-import { CustomInteractionMode } from "../InteractionModes/CustomInteractionMode";
 import { ExampleSceneController } from "src/FinalProject/Examples/Apps/ExampleSceneController";
+import { Color, Mat3, V3 } from "src/anigraph";
+import { MainSceneModel } from "./MainSceneModel";
 
-/**
- * This is your Main Controller class. The scene controller is responsible for managing user input with the keyboard
- * and mouse, as well as making sure that the view hierarchy matches the model heirarchy.
- */
 export class MainSceneController extends ExampleSceneController {
     get model(): MainSceneModel {
         return this._model as MainSceneModel;
     }
 
-    get player() {
-        return this.model.player;
-    }
-
-
     /**
-     * The main customization you might do here would be to set the background color or set a background image.
-     * @returns {Promise<void>}
+     * This is where you specify the mapping from model classes to view classes.
      */
-    async initScene(): Promise<void> {
-        await super.initScene();
-    }
-
-    /**
-     * Specifies what view classes to use for different model class.
-     * If you create custom models and views, you will need to link them here by calling `addModelViewSpec` with the
-     * model class as the first argument and the view class as the second.
-     */
-    initModelViewSpecs() {
+    initModelViewSpecs(): void {
         super.initModelViewSpecs();
-        this.addModelViewSpec(CustomNode1Model, CustomNode1View);
+        this.addExampleModelViewSpecs();
     }
 
-    onAnimationFrameCallback(context: AGLContext) {
-        /**
-         * let's update the model...
-         */
-        this.model.timeUpdate(this.model.clock.time);
-
-        /**
-         * and let's update the controller...
-         * This will mostly update any interactions that depend on time.
-         * Keep in mind that the model and controller run on separate clocks for this, since we may
-         * want to pause our model's clock and continue interacting with the scene (e.g., moving the camera around).
-         */
-        this.timeUpdate();
-
-        /**
-         * Clear the rendering context.
-         * you can also specify which buffers to clear: clear(color?: boolean, depth?: boolean, stencil?: boolean)
-         * ``` this.renderer.clear(false, true); ```
-         */
-        context.renderer.clear();
-
-        // render the scene view
-        context.renderer.render(this.getThreeJSScene(), this.getThreeJSCamera());
+    async initScene(): Promise<void> {
+        await super.initScene()
+        // this.initSkyBoxCubeMap();
+        this.setClearColor(Color.Black());
     }
 
     initInteractions() {
         super.initInteractions();
+        this.initExampleInteractions();
 
-        /**
-         * Add an instance of our custom interaction mode
-         */
-        this.defineInteractionMode(CustomInteractionMode.MODE_NAME, CustomInteractionMode.Create(this));
-        this.setCurrentInteractionMode(ADebugInteractionMode.NameInGUI);
     }
+
 }
